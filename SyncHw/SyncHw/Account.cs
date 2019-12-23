1950240308902bcde4b17f84eece899801cd2657
Наиль Акчурин <golumvicerlid35@gmail.com>
2019-12-23 18:30:55 +0600

    init commit

---
+++ SyncHw/SyncHw/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SyncHw
+{
+    public class Account
+    {
+        public string name;
+        public int cash = 1000;
+        private int transferSum;
+        private object _locker = new object();
+
+        public void Transfer(object accountTo)
+        {
+            lock (_locker)
+            {
+                transferSum = new Random().Next(100);
+                var transferNumber = new Random().Next(10000);
+                Thread.Sleep(5);
+                if (cash < transferSum)
+                {
+                    Console.WriteLine($"У {name} не хватает {transferSum}тг для перевода . ===Номер транзакции: {transferNumber} ");
+                    return;
+                }
+                Console.WriteLine($"Со счёта {name} сняли {transferSum} тг. для перевода {((Account)accountTo).name}.         ===Номер транзакции: {transferNumber}____{name}>>>{((Account)accountTo).name}____{transferSum}");
+                cash -= transferSum;
+                ((Account)accountTo).cash += transferSum;
+                Console.WriteLine($"На счёт {((Account)accountTo).name} поступил перевод от {name} в сумме: {transferSum} тг. ===Номер транзакции: {transferNumber}____{((Account)accountTo).name}<<<{name}____{transferSum}");
+            }
+        }
+        
+    }
+}
---
+++ SyncHw/SyncHw/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace SyncHw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accounts = new ConcurrentDictionary<int, Account>();
+            accounts.TryAdd(0, new Account
+            {
+                name = "Первый"
+            });
+            accounts.TryAdd(1, new Account
+            {
+                name = "Второй"
+            });
+            accounts.TryAdd(2, new Account
+            {
+                name = "Третий"
+            });
+
+            var random = new Random();
+
+            for(var i = 0; i < 1000; i++)
+            {
+                var randFrom = random.Next(3);
+                var randTo = random.Next(3);
+                while(randTo == randFrom)
+                {
+                    randTo = random.Next(3);
+                }
+                var thread = new Thread(accounts[randFrom].Transfer);
+                thread.Start(accounts[randTo]);
+            }
+            Thread.Sleep(5000);
+            Console.WriteLine($"{accounts[0].name} === {accounts[0].cash}");
+            Console.WriteLine($"{accounts[1].name} === {accounts[1].cash}");
+            Console.WriteLine($"{accounts[2].name} === {accounts[2].cash}");
+            Console.ReadKey();
+        }
+    }
+}
